5f387d770ef4690af827e495a9a2e88492560980
Gittinger <jgittin@s1008208.srn.sandia.gov>
2017-11-17 15:49:24 -0700

    project_data gets deleted when a project is deleted now

--- web-server/slycat-couchdb-setup.py
+++ web-server/slycat-couchdb-setup.py
@@ -231,7 +231,7 @@ design = {
           if(doc["type"] != "project_data")
             return;
 
-          emit(doc._id, null);
+          emit(doc["project"], null);
         }
         """,
       },
